54fcdd00ba84e7cd0fcac9ed0d423db51bf04488
Pascal <molin.maths@gmail.com>
2016-12-15 09:10:45 +0100

    intersection tree

--- arb/intersections_tree.c
+++ arb/intersections_tree.c
@@ -9,5 +9,23 @@
 void
 intersection_tree(inter_mat inter, tree_t tree, acb_srcptr x, slong d)
 {
+    slong k, l;
+
+    for (k = 0; k < d - 1; k++)
+    {
+        edge_t ek = tree->e[k];
+        for (l = k; l < d - 1; l++)
+        {
+            edge_t el = tree->e[l];
+            if(el.a == ek.a)
+            {
+                /* case ab.ad */
+            }
+            else if (el.a == ek.b)
+            {
+                /* case ab.bd */
+            }
+        }
+    }
     return;
 }
