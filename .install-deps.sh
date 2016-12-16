ab3d4b1cdb2a974f58f686e84b3dddc53afb5694
Pascal <molin.maths@gmail.com>
2016-12-16 13:50:32 +0100

    fix travis

--- .install-deps.sh
+++ .install-deps.sh
@@ -1,17 +1,19 @@
 DEPS=$HOME/deps
+TMP=$HOME/tmp
+mkdir -p $TMP
 
+cd $TMP
 wget https://github.com/wbhart/flint2/archive/trunk.tar.gz
 tar -xf trunk.tar.gz
 cd flint2-trunk
 ./configure --prefix=$DEPS --disable-static
 make -j4 > /dev/null 2>&1
 make install
-cd ..
 
-wget https://github.com/fredrik-johannson/arb/archive/2.9.0.tar.gz
-tar -xf arb-2.9.0.tar.gz
+cd $TMP
+wget https://github.com/fredrik-johansson/arb/archive/2.9.0.tar.gz
+tar -xf 2.9.0.tar.gz
 cd arb-2.9.0
 ./configure --with-flint=$DEPS --prefix=$DEPS --disable-static
 make -j4 > /dev/null 2>&1
 make install
-cd ..
