cb7fc9308c0ef605dd6f2a1e32717b9c5e85f858
Pascal <molin.maths@gmail.com>
2016-12-16 15:14:32 +0100

    fix path again

--- .install-deps.sh
+++ .install-deps.sh
@@ -1,28 +1,40 @@
+#!/bin/sh
 DEPS=$HOME/deps
 TMP=$HOME/tmp
 mkdir -p $TMP
 
-cd $TMP
-wget http://mpir.org/mpir-2.7.0.tar.bz2
-tar -xf mpir-2.7.0.tar.bz2
-cd mpir-2.7.0
-./configure --enable-gmpcompat --prefix=$DEPS --disable-static
-make -j4 > /dev/null 2>&1
-make install
-
-cd $TMP
-wget http://www.mpfr.org/mpfr-3.1.4/mpfr-3.1.4.tar.bz2
-tar -xf mpfr-3.1.4.tar.bz2
-cd mpfr-3.1.4
-./configure --with-gmp=$DEPS --prefix=$DEPS --disable-static
-make -j4 > /dev/null 2>&1
-make install
+if [[ "TRAVIS_OS_NAME" == "osx" ]]
+then
+    brew update
+    brew install gmp mpfr
+elif [[ "TRAVIS_OS_NAME" == "linux" ]] 
+    sudo apt-get update
+    sudo apt-get install libgmp-dev libmpfr-dev
+else
+    cd $TMP
+    wget http://mpir.org/mpir-2.7.0.tar.bz2
+    tar -xf mpir-2.7.0.tar.bz2
+    cd mpir-2.7.0
+    ./configure --enable-gmpcompat --prefix=$DEPS --disable-static
+    make -j4 > /dev/null 2>&1
+    make install
+    GMP="--with-gmp=$DEPS"
+    
+    cd $TMP
+    wget http://www.mpfr.org/mpfr-3.1.4/mpfr-3.1.4.tar.bz2
+    tar -xf mpfr-3.1.4.tar.bz2
+    cd mpfr-3.1.4
+    ./configure --with-gmp=$DEPS --prefix=$DEPS --disable-static
+    make -j4 > /dev/null 2>&1
+    make install
+    MPFR="--with-mpfr=$DEPS"
+fi
 
 cd $TMP
 wget https://github.com/wbhart/flint2/archive/trunk.tar.gz
 tar -xf trunk.tar.gz
 cd flint2-trunk
-./configure --with-gmp=$DEPS --with-mpfr=$DEPS --prefix=$DEPS --disable-static
+./configure $GMP $MPFR --prefix=$DEPS --disable-static
 make -j4 > /dev/null 2>&1
 make install
 
@@ -30,6 +42,6 @@ cd $TMP
 wget https://github.com/fredrik-johansson/arb/archive/2.9.0.tar.gz
 tar -xf 2.9.0.tar.gz
 cd arb-2.9.0
-./configure --with-gmp=$DEPS --with-mpfr=$DEPS --with-flint=$DEPS --prefix=$DEPS --disable-static
+./configure $GMP $MPFR --with-flint=$DEPS --prefix=$DEPS --disable-static
 make -j4 > /dev/null 2>&1
 make install
