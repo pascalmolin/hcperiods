ab8ffab77ed270796072e44e8517a0b89a5d09d7
Pascal <molin.maths@gmail.com>
2016-12-23 11:45:51 +0100

    test extra d1 in mth root

--- arb/test/t-mth_root.c
+++ arb/test/t-mth_root.c
@@ -17,7 +17,7 @@ int main() {
     {
         for (i = 0; i < 10; i++)
         {
-            slong m, d, j;
+            slong m, d, d1, j;
 
             d = 3 + n_randint(state, 20);
             m = 2 + n_randint(state, 12);
@@ -34,32 +34,36 @@ int main() {
             {
                 arb_randtest_precise(x, state, prec, 3);
                 arb_tanh(x, x, prec);
-                mth_root_pol_def(y1, u, d, d, x, m, prec);
-                mth_root_pol_prod(y2, u, d, d, x, m, prec);
-                if (!acb_overlaps(y1, y2))
+
+                for (d1 = 0; d1 <= d; d1++)
                 {
-                    flint_printf("FAIL:\n\n");
-                    flint_printf("d = %ld, m = %ld, prec = %ld\n", d, m, prec);
-                    flint_printf("mth_root_def = ");
-                    acb_printd(y1, 20);
-                    flint_printf("\nmth_root_pol = ");
-                    acb_printd(y2, 20);
-                    flint_printf("\n\n");
-                    abort();
-                }
-                if (m == 2)
-                {
-                    sqrt_pol_def(y3, u, d, d, x, prec);
-                    if (!acb_overlaps(y1, y3))
+                    mth_root_pol_def(y1, u, d1, d, x, m, prec);
+                    mth_root_pol_prod(y2, u, d1, d, x, m, prec);
+                    if (!acb_overlaps(y1, y2))
                     {
                         flint_printf("FAIL:\n\n");
                         flint_printf("d = %ld, m = %ld, prec = %ld\n", d, m, prec);
                         flint_printf("mth_root_def = ");
                         acb_printd(y1, 20);
-                        flint_printf("\nsqrt_root_def ");
-                        acb_printd(y3, 20);
+                        flint_printf("\nmth_root_pol = ");
+                        acb_printd(y2, 20);
                         flint_printf("\n\n");
                         abort();
+                    }
+                    if (m == 2)
+                    {
+                        sqrt_pol_def(y3, u, d1, d, x, prec);
+                        if (!acb_overlaps(y1, y3))
+                        {
+                            flint_printf("FAIL:\n\n");
+                            flint_printf("d = %ld, m = %ld, prec = %ld\n", d, m, prec);
+                            flint_printf("mth_root_def = ");
+                            acb_printd(y1, 20);
+                            flint_printf("\nsqrt_root_def ");
+                            acb_printd(y3, 20);
+                            flint_printf("\n\n");
+                            abort();
+                        }
                     }
                 }
             }
@@ -72,6 +76,7 @@ int main() {
 
         }
     }
+
     flint_randclear(state);
     flint_cleanup();
     printf("PASS\n");
