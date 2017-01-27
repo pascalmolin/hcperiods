f148568daa3ebccb2569ab042429ffc68cf2d6ab
Pascal <molin.maths@gmail.com>
2017-01-27 11:00:26 +0100

    simplify differentials

--- arb/differentials.c
+++ arb/differentials.c
@@ -13,16 +13,13 @@ holomorphic_differentials(cohom_t dz, slong d, slong m)
     slong i, j, k, delta;
     delta = n_gcd(m, d);
     k = 0;
-    for (j = 1; j < m; j++)
+    for (j = 1 + (m + delta - 1) / d; j < m; j++)
     {
-        for (i = 1; i < d; i++)
+        for (i = 1; i < d && m * i <= j * d - delta; i++)
         {
-            if (j * d >= m * i + delta)
-            {
-                dz[k].x = i - 1;
-                dz[k].y = j;
-                k++;
-            }
+            dz[k].x = i - 1;
+            dz[k].y = j;
+            k++;
         }
     }
     if (2 * k != (d-1)*(m-1) - delta + 1)
