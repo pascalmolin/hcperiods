c1a0eb13156b62a7f8bd9a0897584838897178f7
Christian Neurohr <christian.neurohr@uni-oldenburg.de>
2017-03-31 19:30:56 +0200

    - Same commit? Didn't you see it?

--- arb/integrals_gc.c
+++ arb/integrals_gc.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 
- Copyright (C) 2016 Pascal Molin
+ Copyright (C) 2017 Pascal Molin, Christian Neurohr
 
  ******************************************************************************/
 
@@ -68,35 +68,9 @@ gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int
             acb_printd(res + i, 20);
 #endif
         }
-
-        continue;
-        /* TODO: could reuse -x, but not a big deal */
-
-        if (l == 0)
-            continue;
-
-        /* now on -x */
-        arb_neg(x, x);
-
-        sqrt_pol(y, u, d1, d, x, prec);
-        acb_inv(y, y, prec);
-
-        /* differentials : j = 1 && i < g */
-        acb_set(yxi, y);
-        acb_add(res + 0, res + 0, yxi, prec);
-
-        for (i = 1; i < g; i++)
-        {
-            acb_mul_arb(yxi, yxi, x, prec);
-            if (i % 2)
-                acb_add(res + i, res + i, yxi, prec);
-            else
-                acb_sub(res + i, res + i, yxi, prec);
-        }
     }
 
     /* multiply by weight = Pi / n */
-
     arb_const_pi(w, prec);
     arb_div_ui(w, w, n, prec);
 
