11942678093ae7067cef039b30edaadea8cefc6a
Pascal <molin.maths@gmail.com>
2017-04-05 18:39:27 +0200

    reuse -x in integrals gc

--- arb/integrals_gc.c
+++ arb/integrals_gc.c
@@ -9,7 +9,7 @@
 void
 gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int flag, slong prec)
 {
-    slong i, l;
+    slong l;
     fmpq_t ln;
     arb_t w, x;
     acb_t y, yxi;
@@ -37,71 +37,41 @@ gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int
 
     /* compute integral */
     _acb_vec_zero(res, g);
-    for (l = 0; l < n; l++)
+
+    for (l = 0; 2 * l + 1 < n; l++)
     {
-        fmpq_t ln;
 
         /* compute x */
-        fmpq_init(ln);
         fmpq_set_si(ln, 2 * l + 1, 2 * n);
         arb_cos_pi_fmpq(x, ln, prec);
-        fmpq_clear(ln);
 
         /* compute 1/y(x) */
         sqrt_pol(y, u, d1, d, x, prec);
         acb_inv(y, y, prec);
 
-        /* differentials : j = 1 && i < g */
-        acb_set(yxi, y);
-        acb_add(res + 0, res + 0, yxi, prec);
-#if 0 && DEBUG
-        flint_printf("\nl = %ld, res[%ld] = ", l, 0);
-        acb_printd(res + 0, 20);
-#endif
-
-        for (i = 1; i < g; i++)
-        {
-            acb_mul_arb(yxi, yxi, x, prec);
-            acb_add(res + i, res + i, yxi, prec);
-#if 0 && DEBUG
-            flint_printf("\nl = %ld, res[%ld] = ", l, i);
-            acb_printd(res + i, 20);
-#endif
-        }
-
-        continue;
-        /* TODO: could reuse -x, but not a big deal */
-
-        if (l == 0)
-            continue;
+        /* differentials */
+        acb_vec_add_geom_arb(res, g, y, x, prec);
 
         /* now on -x */
         arb_neg(x, x);
 
         sqrt_pol(y, u, d1, d, x, prec);
         acb_inv(y, y, prec);
+        acb_vec_add_geom_arb(res, g, y, x, prec);
+    }
 
-        /* differentials : j = 1 && i < g */
-        acb_set(yxi, y);
-        acb_add(res + 0, res + 0, yxi, prec);
-
-        for (i = 1; i < g; i++)
-        {
-            acb_mul_arb(yxi, yxi, x, prec);
-            if (i % 2)
-                acb_add(res + i, res + i, yxi, prec);
-            else
-                acb_sub(res + i, res + i, yxi, prec);
-        }
+    if (n % 2)
+    {
+        arb_zero(x);
+        sqrt_pol(y, u, d1, d, x, prec);
+        acb_inv(y, y, prec);
+        acb_add(res + 0, res + 0, y, prec);
     }
 
     /* multiply by weight = Pi / n */
-
     arb_const_pi(w, prec);
     arb_div_ui(w, w, n, prec);
-
-    for (i = 0; i < g; i++)
-        acb_mul_arb(res + i, res + i, w, prec);
+    _acb_vec_scalar_mul_arb(res, res, g, w, prec);
 
     fmpq_clear(ln);
     arb_clear(x);
