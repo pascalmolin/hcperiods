ddfd211cb3956550b26532d8137213c5def1d299
Pascal <molin.maths@gmail.com>
2017-04-05 15:46:56 +0200

    Makefile

--- arb/profile/timings.sh
+++ arb/profile/timings.sh
@@ -3,6 +3,8 @@
 NARG="3 5 8 13 21 30"
 MARG="2 3 4 5 9 13"
 PREC="64 256 1024 4096"
+echo "# extra arg $1"
+echo "# prec; m; n; time"
 for p in $PREC
 do
     for m in $MARG
@@ -10,7 +12,7 @@ do
         for n in $NARG
         do
             printf "$p; $m; $n; "
-            DO="build/example/periods --quiet --bern $n -m $m --prec $p"
+            DO="build/example/periods --bench 1 --bern $n -m $m --prec $p"
             if $DO &> /dev/null; then
                 dumbbench --raw -- $DO
             else
