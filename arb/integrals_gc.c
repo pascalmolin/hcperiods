ab50f3ba144810c1425fdb11c96616ab0bafcf1e
Pascal <molin.maths@gmail.com>
2017-04-06 12:26:06 +0200

    precompute gc once

--- arb/integrals_gc.c
+++ arb/integrals_gc.c
@@ -7,14 +7,13 @@
 #include "abel_jacobi.h"
 
 void
-gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int flag, slong prec)
+gc_integrals_precomp(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, const gc_int_t gc, int flag, slong prec)
 {
     slong l;
     arb_t w, x;
     acb_t y, yxi;
     void (*sqrt_pol) (acb_t y, acb_srcptr u, slong d1, slong d,
             const arb_t x, slong prec);
-    gc_int_t gc;
 
     arb_init(w);
     arb_init(x);
@@ -22,7 +21,7 @@ gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int
     acb_init(yxi);
 #if DEBUG
     flint_printf("\ngc integral : d1 = %ld, d = %ld, g = %ld, n = %ld, prec = %ld",
-            d1, d, g, n, prec);
+            d1, d, g, gc->n, prec);
 #endif
 
     sqrt_pol = &sqrt_pol_def;
@@ -33,8 +32,6 @@ gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int
         sqrt_pol = &sqrt_pol_def;
     else if (flag & AJ_ROOT_TURN)
         sqrt_pol = &sqrt_pol_turn;
-
-    gc_int_init(gc, n, prec);
 
     /* compute integral */
     _acb_vec_zero(res, g);
@@ -57,18 +54,17 @@ gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int
         acb_vec_add_geom_arb(res, g, y, x, prec);
     }
 
-    if (n % 2)
+    if (gc->n % 2)
     {
         arb_zero(x);
         sqrt_pol(y, u, d1, d, x, prec);
         acb_inv(y, y, prec);
         acb_add(res + 0, res + 0, y, prec);
     }
-    gc_int_clear(gc);
 
     /* multiply by weight = Pi / n */
     arb_const_pi(w, prec);
-    arb_div_ui(w, w, n, prec);
+    arb_div_ui(w, w, gc->n, prec);
     _acb_vec_scalar_mul_arb(res, res, g, w, prec);
 
     arb_clear(x);
@@ -78,9 +74,18 @@ gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int
 }
 
 void
-integrals_edge_gc(acb_ptr res, ydata_t ye, sec_t c, slong n, mag_t e, int flag, slong prec)
+gc_integrals(acb_ptr res, acb_srcptr u, slong d1, slong d, slong g, slong n, int flag, slong prec)
 {
-    gc_integrals(res, ye->u, ye->n1, c.n - 2, c.g, n, flag, prec);
+    gc_int_t gc;
+    gc_int_init(gc, n, prec);
+    gc_integrals_precomp(res, u, d1, d, g, gc, flag, prec);
+    gc_int_clear(gc);
+}
+
+void
+integrals_edge_gc(acb_ptr res, ydata_t ye, sec_t c, const gc_int_t gc, mag_t e, int flag, slong prec)
+{
+    gc_integrals_precomp(res, ye->u, ye->n1, c.n - 2, c.g, gc, flag, prec);
     if (0 && e)
         _acb_vec_add_error_mag(res, c.g, e);
     /*integrals_edge_factors_gc(res, ye->ba2, ye->ab, ye->c, c, prec);*/
@@ -92,12 +97,15 @@ integrals_tree_gc(acb_mat_t integrals, sec_t c, const tree_t tree, int flag, slo
 {
     slong k, n;
     mag_t e;
+    gc_int_t gc;
 
     mag_init(e);
     n = gc_params_tree(e, tree, c, prec);
+    gc_int_init(gc, n, prec);
 
     for (k = 0; k < tree->n; k++)
-        integrals_edge_gc(integrals->rows[k], tree->data + k, c, n, e, flag, prec);
+        integrals_edge_gc(integrals->rows[k], tree->data + k, c, gc, e, flag, prec);
 
+    gc_int_clear(gc);
     mag_clear(e);
 }
