c6463f6851ce5e47708bbe6f5a43be035bcd8a72
Pascal <molin.maths@gmail.com>
2017-07-21 15:17:13 +0200

    update compare script

--- doc/timings/compare.sh
+++ doc/timings/compare.sh
@@ -59,7 +59,10 @@ if [ "$method" == "oldmagma" ] || [ "$method" == "newmagma" ]; then
         bench_magma
 elif [ "$method" == "arb" ]; then
     export LD_LIBRARY_PATH=.:$HOME/install/lib:$ARB;
-    dumbbench --raw -- $ARB/build/example/periods --bench 1 --int --$pol $n -m $m --prec $prec
+    if [ { $m -gt 10 && $prec -lt 512 && $n -lt 10 } || $prec -lt 512 || $prec -lt 2000 && $n -lt 10 ]; then
+        dumbbench --raw -- $ARB/build/example/periods --bench 1 --int --$pol $n -m $m --prec $prec
+    else
+        time -f "%S" $ARB/build/example/periods --bench 1 --int --$pol $n -m $m --prec $prec
 else
     echo "$0 <method> <pol> <m> <n> <prec>"
     exit 0
